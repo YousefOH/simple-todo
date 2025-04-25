719c0c3c95da6e1f1107623ef425416a00ad5f8c
YousefOH <97213383+YousefOH@users.noreply.github.com>
2025-04-26 02:38:24 +0300

    Update firebase.js

--- simple-todo/src/firebase.js
+++ simple-todo/src/firebase.js
@@ -1,16 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
-// Replace these values with your own Firebase project config
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCc0bLQHEPRpkcLQOJpd10OKBrKN_spC_Y",
-    authDomain: "simple-todo-5b3cb.firebaseapp.com",
-    databaseURL: "https://simple-todo-5b3cb-default-rtdb.firebaseio.com",
-    projectId: "simple-todo-5b3cb",
-    storageBucket: "simple-todo-5b3cb.firebasestorage.app",
-    messagingSenderId: "109955256832",
-    appId: "1:109955256832:web:0ea1d0bae251fc08ab3ee5"
+    apiKey: "-",
+    authDomain: "-",
+    databaseURL: "-",
+    projectId: "-",
+    storageBucket: "-",
+    messagingSenderId: "-",
+    appId: "-"
   };
   
 // Initialize Firebase
